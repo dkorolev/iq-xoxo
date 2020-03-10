f46b0850b87627039c6588c97d0e0ed4c533c5a6
Dima Korolev <dmitry.korolev@gmail.com>
2020-03-09 18:14:05 -0700

    Enumerated all possible legal placements for the pieces.

--- iq-xoxo.cc
+++ iq-xoxo.cc
@@ -2,15 +2,22 @@
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <set>
 #include <string>
 #include <vector>
 
-#define ASSERT(x) if (!(x)) { std::cerr << __FILE__ << ':' << __LINE__ << ": `" << #x << "` failed.\n"; std::exit(-1); }
+#define ASSERT(x)                                                             \
+  if (!(x)) {                                                                 \
+    std::cerr << __FILE__ << ':' << __LINE__ << ": `" << #x << "` failed.\n"; \
+    std::exit(-1);                                                            \
+  }
 
 constexpr static const char* const kPiecesFile = "pieces.txt";
 constexpr static const char* const kProblemFile = "problem.txt";
 
 int main(int argc, char** argv) {
+  // `legal_placements[t]` lists all possible legal ways to place piece `t` onto the board.
+  std::vector<std::vector<std::vector<std::pair<int, int>>>> legal_placements(10);
   // Read and parse the pieces file.
   {
     std::ifstream fi_pieces(kPiecesFile);
@@ -30,6 +37,93 @@ int main(int argc, char** argv) {
     }
     ASSERT(counts == std::vector<int>(10, 5));  // All pieces are of five cells.
     // NOTE(dkorolev): This does not check whether each piece is continuous, trust the contents of `kPiecesFile`.
+    // Fill the `legal_placements` data structure.
+    for (int t = 0; t < 10; ++t) {
+      std::set<std::set<std::pair<int, int>>> placements_set;  // To exclude duplicate placements.
+
+      char const c = 'A' + t;
+      std::set<std::pair<int, int>> cells;
+      for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 10; ++j) {
+          if (pieces[i][j] == c) {
+            cells.insert({i, j});
+          }
+        }
+      }
+      auto const Mark = [&]() {
+        // Mark all possible legal placements of the piece in its current orientation.
+        // Assume `X`-s and `O`-x are in the right places.
+        for (int di = -22; di <= +22; ++di) {
+          for (int dj = -22; dj <= +22; ++dj) {
+            if (((di + dj) & 1) == 0) {  // Respect parity for `X`-s and `O`-s.
+              bool ok = true;
+              std::set<std::pair<int, int>> placement;
+              for (auto const& cell : cells) {
+                int const i = cell.first + di;
+                int const j = cell.second + dj;
+                if (i >= 0 && i < 5 && j >= 0 && j < 10) {
+                  placement.insert({i, j});
+                } else {
+                  ok = false;
+                }
+              }
+              if (ok) {
+                if (!placements_set.count(placement)) {
+                  placements_set.insert(placement);
+                  legal_placements[t].push_back(
+                      std::vector<std::pair<int, int>>(std::begin(placement), std::end(placement)));
+#if 0
+                  // NOTE(dkorolev): Visualize all possible placements for this piece.
+                  if (c == 'E') {
+                    std::vector<std::string> tmp(5, std::string(10, '.'));
+                    for (auto const& cell : placement) {
+                      tmp[cell.first][cell.second] = c;
+                    }
+                    for (int i = 0; i < 5; ++i) {
+                      std::cerr << tmp[i] << std::endl;
+                    }
+                    std::cerr << std::endl;
+                  }
+#endif
+                }
+              }
+            }
+          }
+        }
+      };
+      auto const Rotate90 = [&]() {
+        std::set<std::pair<int, int>> new_cells;
+        for (auto const& cell : cells) {
+          new_cells.insert({-cell.second, cell.first});
+        }
+        cells = std::move(new_cells);
+      };
+      auto const FlipOver = [&]() {
+        std::set<std::pair<int, int>> new_cells;
+        for (auto const& cell : cells) {
+          // `+1` to swap `X`-s and `O`-s when flipping the piece over.
+          new_cells.insert({cell.second, cell.first + 1});
+        }
+        cells = std::move(new_cells);
+      };
+      // Mark all rotations and flips.
+      Mark();
+      Rotate90();
+      Mark();
+      Rotate90();
+      Mark();
+      Rotate90();
+      Mark();
+      FlipOver();
+      Mark();
+      Rotate90();
+      Mark();
+      Rotate90();
+      Mark();
+      Rotate90();
+      Mark();
+      // std::cerr << "Piece `" << c << "`: " << legal_placements[t].size() << " legal placements.\n";
+    }
   }
   std::vector<std::string> board(5);
   std::vector<bool> allowed_pieces(10, true);
